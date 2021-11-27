496c713dd735b284a49af002bdbd62abd3a92560
Haneul Lee <lovesky4294@gmail.com>
2021-11-27 18:26:40 +0900

    test as, attrs of styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import styled from "styled-components";
 
+const Btn = styled.button`
+  color: white;
+  background-color: #4caf50; /* Green */
+  border: 0;
+  border-radius: 0.25rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: red;
+`;
+
 const App = function () {
-  return <div className="App" />;
+  return (
+    <Wrapper as="header">
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
+      <Input />
+      <Input />
+      <Input />
+    </Wrapper>
+  );
 };
 
 export default App;
