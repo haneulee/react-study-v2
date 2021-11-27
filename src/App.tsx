6404226752d58beedaf5a2ce5f6ecd776bf26e7f
Haneul Lee <lovesky4294@gmail.com>
2021-11-27 18:43:03 +0900

    pseudo selector & animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
+`;
 
 const Btn = styled.button`
   color: white;
   background-color: #4caf50; /* Green */
   border: 0;
   border-radius: 0.25rem;
+`;
+
+const Emoji = styled.span`
+  font-size: 30px;
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 300px;
+  background-color: skyblue;
+  animation: ${animation} 1s linear infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 50px;
+      &:active {
+        opacity: 0;
+      }
+    }
+  }
 `;
 
 const Wrapper = styled.div`
@@ -18,13 +55,16 @@ const Input = styled.input.attrs({ required: true })`
 const App = function () {
   return (
     <Wrapper as="header">
-      <Btn>Log in</Btn>
-      <Btn as="a" href="/">
-        Log in
-      </Btn>
-      <Input />
-      <Input />
-      <Input />
+      <Box>
+        <Emoji as="p">😂</Emoji>
+      </Box>
+      <div>
+        <Emoji>🔥</Emoji>
+        <Btn as="a" href="/">
+          Log in
+        </Btn>
+        <Input />
+      </div>
     </Wrapper>
   );
 };
