9d0054a09fbe15098714481b6d6dbdacf55d03b4
Haneul Lee <lovesky4294@gmail.com>
2021-12-08 23:26:15 +0900

    add coin API & Coins Component style

--- src/theme.ts
+++ src/theme.ts
@@ -3,13 +3,13 @@ import { DefaultTheme } from "styled-components";
 export const lightTheme: DefaultTheme = {
   bgColor: "#2f3640",
   textColor: "#f5f6fa",
-  accentColor: "#4cd137",
+  accentColor: "#9c88ff",
   btnColor: "tomato",
 };
 
 export const darkTheme: DefaultTheme = {
   bgColor: "black",
   textColor: "white",
-  accentColor: "#4cd137",
+  accentColor: "#9c88ff",
   btnColor: "teal",
 };
