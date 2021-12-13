c28bcffaa6ef6a71d7d6680b71b18c58320cf6a4
Haneul Lee <lovesky4294@gmail.com>
2021-12-13 16:57:14 +0900

    change ToDo form

--- src/routes/ToDoList.tsx
+++ src/routes/ToDoList.tsx
@@ -1,98 +1,90 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import styled from "styled-components";
+import { atom, useRecoilState } from "recoil";
+import { Helmet } from "react-helmet";
+import ToggleButton from "components/ToggleButton";
+import { useLocation } from "react-router";
+import BackButton from "components/BackButton";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+  margin: 30px;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  height: 100px;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  max-width: 1000px;
+  margin: 30px auto;
+  & input {
+    color: black;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+`;
 
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string;
 }
 
-const ToDoList = function () {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "Password are not the same" },
-        { shouldFocus: true },
-      );
-    }
-    // setError("extraError", { message: "Server offline." });
-  };
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
 
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+const ToDoList = function () {
+  const { state } = useLocation<RouteState>();
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
   return (
-    <div style={{ color: "black" }}>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com emails allowed",
-            },
-          })}
-          placeholder="Email"
-        />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", {
-            required: "write here",
-            validate: {
-              noNico: (value) =>
-                value.includes("nico") ? "no nicos allowed" : true,
-              noNick: (value) =>
-                value.includes("nick") ? "no nick allowed" : true,
-            },
-          })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register("lastName", { required: "write here" })}
-          placeholder="Last Name"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("username", { required: "write here", minLength: 10 })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", { required: "write here", minLength: 5 })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", {
-            required: "Password is required",
-            minLength: {
-              value: 5,
-              message: "Your password is too short.",
-            },
-          })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message}</span>
-        <button type="button">Add</button>
-        <span>{errors?.extraError?.message}</span>
-      </form>
-    </div>
+    <Container>
+      <Helmet>
+        <title>To Do List ✅</title>
+      </Helmet>
+      <Header>
+        <BackButton />
+        <Title>To Do List ✅</Title>
+        <ToggleButton />
+      </Header>
+      <Wrapper>
+        <form onSubmit={handleSubmit(handleValid)}>
+          <input
+            {...register("toDo", {
+              required: "Please write a To Do",
+            })}
+            placeholder="Write a to do"
+          />
+          <button>Add</button>
+        </form>
+        <ul>
+          {toDos.map((toDo) => (
+            <li key={toDo.id}>{toDo.text}</li>
+          ))}
+        </ul>
+      </Wrapper>
+    </Container>
   );
 };
 
